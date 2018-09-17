68906ed726bd0ed60e43f3f1c9db98ccdf9a86d5
Vivek Verma <vivnps.verma@gmail.com>
2018-09-16 19:38:02 -0700

    Fixing Bugs

--- SmallQuestions/zombie.py
+++ SmallQuestions/zombie.py
@@ -11,42 +11,11 @@ for i in range(0, len(field), 1):
         if field[i][j] == "Z":
             x = j
             y = i
-"""
-def spread_right(f, xpos, ypos):
-    if f[xpos][ypos + 1] == ".":
-        temp = list(f[xpos])
-        temp[ypos + 1] = "Z"
-        temp = "".join(temp)
-        f[xpos] = temp
-        ypos = ypos + 1
-    elif f[xpos][ypos + 1] == "W" or f[xpos + 1][ypos] == "W":
-        return f
-    elif f[xpos][ypos + 1] == "H" or f[xpos + 1][ypos] == "H":
-        return f
-
-    return spread_right(f, xpos, ypos)
-
-
-def spread_top(f, xpos, ypos):
-    if f[xpos + 1][ypos] == ".":
-        temp = list(f[xpos + 1])
-        temp[ypos] = "Z"
-        temp = "".join(temp)
-        f[xpos + 1] = temp
-        xpos = xpos + 1
-    elif f[xpos][ypos + 1] == "W" or f[xpos + 1][ypos] == "W":
-        return f
-    elif f[xpos][ypos + 1] == "H" or f[xpos + 1][ypos] == "H":
-        return f
-   
-    return spread_top(f, xpos, ypos)
-"""
 
 
 def spread(f, xpos, ypos):
     li = []
-
-    if 0 < xpos < 14:
+    try:
         if f[xpos + 1][ypos] == ".":
             temp = list(f[xpos + 1])
             temp[ypos] = "Z"
@@ -59,7 +28,6 @@ def spread(f, xpos, ypos):
             temp = "".join(temp)
             f[xpos - 1] = temp
             li.append([xpos - 1, ypos])
-    if 0 < ypos < 39:
         if f[xpos][ypos - 1] == ".":
             temp = list(f[xpos])
             temp[ypos - 1] = "Z"
@@ -67,12 +35,13 @@ def spread(f, xpos, ypos):
             f[xpos] = temp
             li.append([xpos, ypos - 1])
         if f[xpos][ypos + 1] == ".":
-            print(ypos, xpos)
             temp = list(f[xpos])
             temp[ypos + 1] = "Z"
             temp = "".join(temp)
             f[xpos] = temp
             li.append([xpos, ypos + 1])
+    except IndexError:
+        pass
     for k in li:
         f = spread(f, k[0], k[1])
     return f
