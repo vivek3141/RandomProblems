8c0e6a765d0d2e5add2aa9a79bcfbb5a03b3ee03
Vivek Verma <vivnps.verma@gmail.com>
2018-11-01 12:51:20 -0700

    Updating Graphs

--- SmallQuestions/force_graph.py
+++ SmallQuestions/force_graph.py
@@ -1,20 +1,17 @@
 import matplotlib.pyplot as plt
-from scipy.interpolate import spline
+from scipy.optimize import curve_fit
 import numpy as np
 
-x1 = [0.618, 1.07, 1.548]
-y1 = [0.49, 0.98, 1.47]
+x1 = [4.58, 9.48, 14.386]
+y1 = [1.47, 2.45, 4.41]
 slope = (y1[0] - y1[2]) / (x1[0] - x1[2])
 
 
-def func(x, a, b):
-    return (a * x) + b
-
-
 def plot(x1, y1, color):
+    i = [1,1]
+
     x = np.linspace(min(x1), max(x1), 300)
-    power_smooth = spline(x1, y1, x)
-    plt.plot(x, power_smooth, color=color)
+    plt.plot(x, line, color=color)
 
 
 plt.title("Acceleration vs Accelerating Force")
@@ -23,5 +20,5 @@ plt.ylabel("Accelerating Force (N)")
 plt.scatter(x1, y1, color="b")
 plt.plot(x1, y1, color="b", label="y = {}x + {}".format(round(slope, 2), round(y1[0] - slope * x1[0], 2)))
 plt.legend(loc="upper left")
-plt.savefig("acceleration.png")
+plt.savefig("force_1.png")
 plt.show()
--- SmallQuestions/mass.py
+++ SmallQuestions/mass.py
@@ -1,23 +1,29 @@
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
+import matplotlib
 
 x1 = [0.95, 1.317, 1.742]
 y1 = [1.548, 1.116, 0.844]
 
 
-def func(x, a, b, c):
-    return a + b * np.exp(c * x)
+def func(x, a, b):
+    return a / (b * x)
 
 
 def plot(x_data, y_data, color):
-    initial = [1, 1, -1.2]
+    initial = [1, 1]
     t = curve_fit(func, x_data, y_data, initial)
     print(t[0])
     xd = np.linspace(min(x_data), max(x_data), 100)
     fit = [func(x, *t[0]) for x in xd]
-    print("{} + {}e^{}x".format(*list(map(lambda x: round(x,2), t[0]))))
-    plt.plot(xd, fit, color=color, label="$0.55 + 4.34e^{-1.55x}$")
+    rounded = list(map(lambda x: round(x, 2), t[0]))
+    print("{}/({}x)".format(*rounded))
+    font = {'family': 'normal',
+            'size': 16}
+
+    matplotlib.rc('font', **font)
+    plt.plot(xd, fit, color=color, label="$\\frac{" + str((rounded[0])) + "}{" + str((rounded[1])) + "x}$")
 
 
 plt.title("Total Mass vs Acceleration")
