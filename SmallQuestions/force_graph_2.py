c63759d39fb2d64925f6a6296a1dd222e7ac72b4
Vivek Verma <vivnps.verma@gmail.com>
2018-11-01 12:59:16 -0700

    Updating Graphs

--- SmallQuestions/force_graph_2.py
+++ SmallQuestions/force_graph_2.py
@@ -3,7 +3,7 @@ from scipy.optimize import curve_fit
 import numpy as np
 
 x1 = [4.58, 9.48, 14.386]
-y1 = [1.47, 2.45, 4.41]
+y1 = [0.882, 1.764, 2.646]
 slope = (y1[0] - y1[2]) / (x1[0] - x1[2])
 
 
@@ -25,8 +25,8 @@ def plot(x_data, y_data, color):
 plt.title("Acceleration vs Accelerating Force")
 plt.xlabel(r"Acceleration $(\frac{m}{s^2})$")
 plt.ylabel("Accelerating Force (N)")
-plt.scatter(x1, y1, color="b")
-plot(x1, y1, color="b")
+plt.scatter(x1, y1, color="g")
+plot(x1, y1, color="g")
 plt.legend(loc="upper left")
-plt.savefig("force_1.png")
+plt.savefig("force_2.png")
 plt.show()
