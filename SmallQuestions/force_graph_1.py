be895ebe0770d305d5bb6feef633916c3025a460
Vivek Verma <vivnps.verma@gmail.com>
2018-11-01 12:54:48 -0700

    Updating Graphs

--- SmallQuestions/force_graph_1.py
+++ SmallQuestions/force_graph_1.py
@@ -7,18 +7,26 @@ y1 = [1.47, 2.45, 4.41]
 slope = (y1[0] - y1[2]) / (x1[0] - x1[2])
 
 
-def plot(x1, y1, color):
-    i = [1,1]
+def func(x, a, b):
+    return (x * a) + b
 
-    x = np.linspace(min(x1), max(x1), 300)
-    plt.plot(x, line, color=color)
+
+def plot(x_data, y_data, color):
+    i = [1, 1]
+    t = curve_fit(func, x_data, y_data, i)
+    print(t[0])
+    xd = np.linspace(min(x_data), max(x_data), 100)
+    line = [func(x, *t[0]) for x in xd]
+    rounded = list(map(lambda x: round(x, 2), t[0]))
+    l = "{}x+ {}".format(*rounded)
+    plt.plot(xd, line, color=color, label=l)
 
 
 plt.title("Acceleration vs Accelerating Force")
 plt.xlabel(r"Acceleration $(\frac{m}{s^2})$")
 plt.ylabel("Accelerating Force (N)")
 plt.scatter(x1, y1, color="b")
-plt.plot(x1, y1, color="b", label="y = {}x + {}".format(round(slope, 2), round(y1[0] - slope * x1[0], 2)))
+plot(x1, y1, color="b")
 plt.legend(loc="upper left")
 plt.savefig("force_1.png")
 plt.show()
